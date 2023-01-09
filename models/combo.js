7de448a89b97877ed4aa82e7c6c3bc3d8a8ca856
RedRaptor10 <weiminh_lam@yahoo.com>
2023-01-09 15:33:44 -0600

    Update combo model

--- models/combo.js
+++ models/combo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ComboSchema = new Schema({
-    damage: { type: mongoose.Decimal128, required: true },
+    damage: { type: mongoose.Types.Decimal128, required: true },
     date: { type: Date, required: true },
     input: { type: String, minLength: 1, maxLength: 500, required: true },
     notes: { type: String, maxLength: 500 },
