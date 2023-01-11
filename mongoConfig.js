ec8af5c6aecb5d520256b7069c31f9aaccc63c8d
RedRaptor10 <weiminh_lam@yahoo.com>
2023-01-11 12:58:03 -0600

    Add database name to mongoDB connection string

--- mongoConfig.js
+++ mongoConfig.js
@@ -5,7 +5,7 @@ const nconf = require('nconf');
 nconf.argv().env().file({ file: './config.json' });
 
 // Set up mongoose connection
-const mongoDB = 'mongodb+srv://' + nconf.get('MONGODB_USERNAME') + ':' + nconf.get('MONGODB_PASSWORD') + '@cluster0.okx3gdx.mongodb.net/?retryWrites=true&w=majority';
+const mongoDB = 'mongodb+srv://' + nconf.get('MONGODB_USERNAME') + ':' + nconf.get('MONGODB_PASSWORD') + '@cluster0.okx3gdx.mongodb.net/combo-list?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
