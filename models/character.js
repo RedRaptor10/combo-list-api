16bfb0c3fcfbab751712df9db7ea2116b0cc45b0
RedRaptor10 <weiminh_lam@yahoo.com>
2023-03-06 14:14:13 -0600

    Update character model

--- models/character.js
+++ models/character.js
@@ -2,18 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CharacterSchema = new Schema({
-    name: {
-        type: String,
-        minLength: 1,
-        maxLength: 20,
-        required: true
-    },
-    slug: {
-        type: String,
-        minLength: 1,
-        maxLength: 20,
-        required: true
-    }
+    name: { type: String, minLength: 1, maxLength: 20, required: true },
+    slug: { type: String, minLength: 1, maxLength: 20, required: true }
 });
 
 module.exports = mongoose.model('Character', CharacterSchema);
